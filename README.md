528b8e0a0a5b08ee1378e6c212e3140460a01ecc
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-11-23 15:42:14 -0800

    Given some JSON data, plot transactions on a map

---
+++ README.md
@@ -0,0 +1,6 @@
+Sinatra application that maps credit card transactions
+
+Plaid API => Transaction data
+Mapbox API => Mapping
+
+Run flannel.rb
--- flannel.rb
+++ flannel.rb
@@ -2,31 +2,52 @@ require "sinatra"
 require "json"
 require "sinatra/reloader" if development?
 require "dotenv"
+require 'pry' if development?
+
 Dotenv.load
 
 get "/" do
-  @geojson_transactions = [
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [37.76, -122.42]
-      },
-      properties: {
-        title: "yay"
-      }
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [-122.42, 37.76]
-      },
-      properties: {
-      title: "kebabs!"
-    }
-  }
-  ].to_json
+  @geojson_transactions = transactions.to_json
 
   erb :index
+end
+
+private
+
+def transactions
+  transaction_data = JSON.parse(File.read("data/transactions.json"))
+  transaction_data["transactions"].map do |transaction|
+    raw_coordinates = transaction["meta"]["location"]["coordinates"]
+
+    if raw_coordinates
+      lat = raw_coordinates["lat"] || 0
+      lng = raw_coordinates["lng"] || 0
+      name = transaction["name"]
+      amount = transaction["amount"]
+
+      if amount > 20
+        marker_size = "large"
+      elsif amount > 10
+        marker_size = "medium"
+      else
+        marker_size = "small"
+      end
+
+      create_feature(lat, lng, name, marker_size)
+    end
+  end.compact
+end
+
+def create_feature(lat, lng, name, marker_size)
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [lng, lat]
+    },
+    properties: {
+      title: name,
+      "marker-size" => marker_size
+    }
+  }
 end
